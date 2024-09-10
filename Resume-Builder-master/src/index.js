429e9968209a646b8bb38a89a136c998466f983d
userone <19cp.ajaykapuriya@adit.ac.in>
2024-09-10 12:27:28 +0530

    Update first

---
+++ Resume-Builder-master/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import setupLocatorUI from "@locator/runtime";
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+if (process.env.NODE_ENV === "root") {
+  setupLocatorUI();
+}
+
+root.render(
+  <React.StrictMode>
+    <ChakraProvider >
+      <Router>
+          <App />
+      </Router>
+    </ChakraProvider>
+  </React.StrictMode>
+);
